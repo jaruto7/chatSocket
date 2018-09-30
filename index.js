b7676f2414233e15a5d99b6180bf0aa9dc1ff821
Jarek Kajko <jaruto7@gmail.com>
2018-09-30 22:08:22 +0200

    Create simple chat

---
+++ index.js
@@ -0,0 +1,61 @@
+// Zaladuj odpowiednie moduly / zaleznosci
+const express = require( 'express' );
+const http = require( 'http' );
+const socketIo = require( 'socket.io' );
+
+// Stworz aplikacje express
+const app = express();
+
+// Stworz serwer HTTP na podstawie aplikacji express
+const server = http.createServer( app );
+
+// Podepnij socket.io do utworzonego uprzednio serwera
+const io = socketIo( server );
+const UsersService = require( '/UsersService' );
+
+const UsersService = new UsersService();
+
+// Ustaw w aplikacji express miejsce "/public" z ktorego beda serwowowane pliki
+app.use( express.static( `${ __dirname }/public` ) );
+
+// Skonfiguruj routing ktory bedzie nasluchiwal na "/" a w odpowiedzi odsylal plik index.html
+app.get( '/', ( req, res ) => {
+    res.sendFile( `${ __dirname }/index.html` );
+});
+
+io.on( 'connection', ( socket ) => {
+    socket.on( 'join', ( name ) => {
+        usersService.addUser ({
+            if: socket.id,
+            name
+        });
+
+        io.emit( 'update', {
+            users: usersService.getAllUsers()
+        });
+    });
+});
+
+io.on( 'connection', ( socket ) => {
+    socket.on( 'disconnect', () => {
+        usersService.removeUser( socket.id );
+        socket.broadcast.emit( 'update', {
+            users: usersService.getAllUsers()
+        });
+    });
+});
+
+io.on( 'connection', ( socket ) => {
+    socket.on( 'message', ( message ) => {
+        const { name } = usersService.getUserById( socket.id );
+        socket.broadcast.emit( 'message', {
+            text: message.text,
+            from: name
+        });
+    });
+})
+
+// Uruchom serwer i nasluchuj na zapytania przychodzace od klientow
+server.listen( 3000, () => {
+    console.log( 'listening on *:3000' );
+});
---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require( 'path' );
+const htmlWebpackPlugin = require( 'html-webpack-plugin' );
+
+// webpack.config.js
+module.exports = ( env ) => {
+    return {
+        mode: env || 'production',
+        entry: './src/index.js',
+        plugins: [new htmlWebpackPlugin({
+            template: 'client/index.html',
+            filename: 'index.html',
+            inject: 'body'
+        })],
+        output: {
+            path: path.resolve( __dirname, 'build' ),
+            filename: 'app.bundle.js'
+        },
+        entry: './client.index.js',
+        output: {
+            path: path.resolve( __dirname, 'public' ),
+            filename: 'app.bundle.js'
+        },
+        devServer: {
+            proxy: {
+                'socket.io': {
+                    target: 'http://localhost:3000',
+                    ws: true
+                }
+            }
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    loader: "babel-loader",
+                    options: {
+                        plugins: env !== 'production' ? ["react-hot-loader/babel"] : []
+                    }
+                },
+                {
+                    test: /\.css$/,
+                    use: [
+                        { loader: 'style-loader' },
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                modules: true
+                            }
+                        }
+                    ]
+                }
+            ]
+        },
+        
+    }
+};
